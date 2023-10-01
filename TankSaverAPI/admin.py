5da9a39d73bbae9ff93408fc7f4aa0ccd67e2224
gustavobalbino-devcit <gustavo.balbino@citechnology.com.br>
2023-10-01 20:17:45 -0300

    Add models to the admin page

--- TankSaverAPI/admin.py
+++ TankSaverAPI/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.apps import apps
 
 # Register your models here.
+
+my_models = apps.get_app_config('TankSaverAPI').get_models()
+
+for model in my_models:
+    try:
+        admin.site.register(model)
+    except admin.sites.AlreadyRegistered:
+        pass
