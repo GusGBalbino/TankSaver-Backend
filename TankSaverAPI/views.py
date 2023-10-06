f0d1ccf6821581646a6f3d813a4a3ba0a36c3f2e
GusGBalbino <gustavo.gbalbino@hotmail.com>
2023-10-05 21:09:32 -0300

    Changes on models

--- TankSaverAPI/views.py
+++ TankSaverAPI/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
-
-# Create your views here.
+from . import models
+from . import serializers
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
