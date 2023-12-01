372eb85d51638aa35a353b68a55a31f4f82a8b37
Gustavo <gustavourio@gmail.com>
2023-12-01 18:06:18 -0300

    alterando porta

--- TankSaver/settings.py
+++ TankSaver/settings.py
@@ -1,10 +1,11 @@
 from pathlib import Path
 from decouple import config
 from datetime import timedelta
+from django.core.management.commands.runserver import Command as runserver
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+runserver.default_port = "3000"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
